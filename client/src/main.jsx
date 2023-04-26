3317082dd0fb0aeffa1897a2903087147eaaed53
izharj <eezhar@gmail.com>
2023-04-26 19:40:42 +0800

    clean up files

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,17 +5,12 @@ import "./index.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { store } from "./app/store.js";
-import { Provider } from "react-redux";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
