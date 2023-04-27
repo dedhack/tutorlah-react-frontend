61254b58c8904fcfd224c601cfb9836ec66d2617
izharj <eezhar@gmail.com>
2023-04-27 14:45:53 +0800

    updated navigation bar to change buttons when user is logged in: display profile and logout button

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import { BeakerIcon } from "@heroicons/react/24/solid";
+import useAuth from "../hooks/useAuth";
+
 const NavigationBar = () => {
+  const { auth } = useAuth();
+
   return (
     <>
       <header aria-label="Site Header" className="bg-white">
@@ -71,22 +75,39 @@ const NavigationBar = () => {
             </nav>
 
             <div className="flex items-center gap-4">
-              <div className="sm:flex sm:gap-4">
-                <NavLink
-                  className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
-                  to="/login"
-                >
-                  Login
-                </NavLink>
+              {/* Conditional display if user already logged in */}
+              {auth ? (
+                <div className="sm:flex sm:gap-4">
+                  <NavLink
+                    className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
+                    to="/profile"
+                  >
+                    Profile
+                  </NavLink>
+                  <NavLink
+                    className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
+                    to="/logout"
+                  >
+                    Logout
+                  </NavLink>
+                </div>
+              ) : (
+                <div className="sm:flex sm:gap-4">
+                  <NavLink
+                    className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
+                    to="/login"
+                  >
+                    Login
+                  </NavLink>
 
-                <NavLink
-                  className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
-                  to="/register"
-                >
-                  Register
-                </NavLink>
-              </div>
-
+                  <NavLink
+                    className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
+                    to="/register"
+                  >
+                    Register
+                  </NavLink>
+                </div>
+              )}
               <button className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
                 <span className="sr-only">Toggle menu</span>
                 <svg
