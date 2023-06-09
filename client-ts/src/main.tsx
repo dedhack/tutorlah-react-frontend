d5bf8198a95cfb3aae0aa1ad53170e2c502c79df
Izhar Bin Jumadi <eezhar@gmail.com>
2023-06-09 15:27:59 +0800

    added dependecies

--- client-ts/src/main.tsx
+++ client-ts/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
